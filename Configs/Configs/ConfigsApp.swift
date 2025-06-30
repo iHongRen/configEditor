8bd1ff7d4b40668b924b43dc8f06d404ebaa78aa
chenxianyin <iamhongren@gmail.com>
2025-06-30 13:51:28 +0800

    fix: hide menu

--- Configs/Configs/ConfigsApp.swift
+++ Configs/Configs/ConfigsApp.swift
@@ -69,6 +69,31 @@ struct ConfigsApp: App {
     }
     
     init() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            Self.hideDefaultMenuItems()
+        }
         NSWindow.allowsAutomaticWindowTabbing = false
     }
+    
+    private static func hideDefaultMenuItems() {
+            guard let mainMenu = NSApplication.shared.mainMenu else { return }
+            
+            for item in mainMenu.items {
+                if ["File", "Edit", "View", "Window"].contains(item.title) {
+                    item.isHidden = true
+                }
+                
+                if item.title == "Configs" {
+                    if let submenu = item.submenu {
+                        for subItem in submenu.items {
+                            if ["About Configs", "Quit Configs"].contains(subItem.title) {
+                                subItem.isHidden = false
+                            } else {
+                                subItem.isHidden = true
+                            }
+                        }
+                    }
+                }
+            }
+        }
 }
