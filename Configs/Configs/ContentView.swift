85051b1d8abdfa4d49f6357d26b1fe38d64e4eeb
chenxianyin <iamhongren@gmail.com>
2025-07-07 11:00:55 +0800

    refactor:  CommonConfigs

--- Configs/Configs/ContentView.swift
+++ Configs/Configs/ContentView.swift
@@ -90,49 +90,12 @@ struct ContentView: View {
     @State private var editorMatchCount: Int = 0
     @State private var editorCurrentMatchIndex: Int = 0
     
-    // 添加右键菜单状态
     @State private var contextMenuFile: ConfigFile?
     @State private var showDeleteAlert = false
     
     // Automatically scan user's home directory for all hidden config files and common config directories
 
-    // Common development configuration files covering mainstream programming languages and development tools
-    let commonConfigs = [
-        // Shell 
-        (".zshrc", ".zshrc"), (".bashrc", ".bashrc"), (".bash_profile", ".bash_profile"), (".profile", ".profile"), (".zprofile", ".zprofile"), (".zshenv", ".zshenv"), (".zlogin", ".zlogin"), (".zlogout", ".zlogout"), (".inputrc", ".inputrc"), (".dir_colors", ".dir_colors"), (".tmux.conf", ".tmux.conf"), (".screenrc", ".screenrc"), (".nanorc", ".nanorc"), (".fzf.zsh", ".fzf.zsh"), (".fzf.bash", ".fzf.bash"),
-        // Git 
-        (".gitconfig", ".gitconfig"), (".gitignore", ".gitignore"), (".gitattributes", ".gitattributes"), (".hgignore", ".hgignore"), (".gitmodules", ".gitmodules"),
-        // SSH & GPG
-        (".ssh/config", ".ssh/config"), (".gnupg/gpg.conf", ".gnupg/gpg.conf"), (".gnupg/gpg-agent.conf", ".gnupg/gpg-agent.conf"),
-        // eidt
-        (".vimrc", ".vimrc"), (".viminfo", ".viminfo"), (".emacs", ".emacs"), (".spacemacs", ".spacemacs"), (".config/Code/User/settings.json", ".config/Code/User/settings.json"), (".config/Code/User/keybindings.json", ".config/Code/User/keybindings.json"), (".editorconfig", ".editorconfig"),
-        // Node.js & fe
-        (".npmrc", ".npmrc"), (".yarnrc", ".yarnrc"), (".nvmrc", ".nvmrc"), (".npmignore", ".npmignore"), (".prettierrc", ".prettierrc"), (".prettierignore", ".prettierignore"), (".eslintrc", ".eslintrc"), (".eslintrc.json", ".eslintrc.json"), (".eslintignore", ".eslintignore"), (".stylelintrc", ".stylelintrc"), (".stylelintignore", ".stylelintignore"), (".babelrc", ".babelrc"), (".babelrc.js", ".babelrc.js"), (".parcelrc", ".parcelrc"), (".mocharc.json", ".mocharc.json"), (".mocharc.js", ".mocharc.js"),
-        // Python
-        (".pythonrc", ".pythonrc"), (".pypirc", ".pypirc"), (".condarc", ".condarc"), (".jupyter/jupyter_notebook_config.py", ".jupyter/jupyter_notebook_config.py"), (".ipython/profile_default/ipython_config.py", ".ipython/profile_default/ipython_config.py"),
-        // Ruby
-        (".irbrc", ".irbrc"), (".pryrc", ".pryrc"), (".gemrc", ".gemrc"), (".railsrc", ".railsrc"), (".rspec", ".rspec"), (".rubocop.yml", ".rubocop.yml"), (".ruby-version", ".ruby-version"), (".ruby-gemset", ".ruby-gemset"), (".bundle/config", ".bundle/config"),
-        // Java
-        (".m2/settings.xml", ".m2/settings.xml"), (".gradle/gradle.properties", ".gradle/gradle.properties"), (".gradle/gradle-wrapper.properties", ".gradle/gradle-wrapper.properties"),
-        // Go
-        (".goenv", ".goenv"), (".gorc", ".gorc"),
-        // Rust
-        (".cargo/config", ".cargo/config"), (".cargo/credentials", ".cargo/credentials"),
-        // PHP
-        (".phpenv", ".phpenv"), (".php.ini", ".php.ini"),
-        // C/C++
-        (".clang-format", ".clang-format"), (".clang-tidy", ".clang-tidy"), (".gdbinit", ".gdbinit"), (".lldbinit", ".lldbinit"),
-        // R
-        (".Rprofile", ".Rprofile"), (".Renviron", ".Renviron"), (".Rhistory", ".Rhistory"),
-        // Docker
-        (".docker/config.json", ".docker/config.json"), (".dockerignore", ".dockerignore"), (".compose.yaml", ".compose.yaml"), ("docker-compose.yml", "docker-compose.yml"),
-        // db
-        (".my.cnf", ".my.cnf"), (".psqlrc", ".psqlrc"), (".pgpass", ".pgpass"), (".sqliterc", ".sqliterc"),
-        // other
-        (".env", ".env"), (".env.local", ".env.local"), (".env.production", ".env.production"), (".env.development", ".env.development"), (".env.test", ".env.test"), (".agignore", ".agignore"), (".ackrc", ".ackrc"), (".rsyncrc", ".rsyncrc"), (".wgetrc", ".wgetrc"), (".curlrc", ".curlrc"), (".lscolors", ".lscolors"), (".lesshst", ".lesshst"), (".node_repl_history", ".node_repl_history"), (".bash_history", ".bash_history"), (".zsh_history", ".zsh_history"), (".mysql_history", ".mysql_history"), (".psql_history", ".psql_history"), (".sqlite_history", ".sqlite_history"),
-        // tool
-        (".config/starship.toml", ".config/starship.toml"), (".config/alacritty/alacritty.yml", ".config/alacritty/alacritty.yml"), (".config/kitty/kitty.conf", ".config/kitty/kitty.conf"), (".config/fish/config.fish", ".config/fish/config.fish")
-    ]
+    
 
     private func saveAllConfigs() {
         let configDicts = configFiles.map { $0.toDictionary() }
@@ -159,18 +122,7 @@ struct ContentView: View {
         }
     }
 
-    private func scanForDefaultConfigFiles() -> [ConfigFile] {
-        let homePath = NSHomeDirectory()
-        let fileManager = FileManager.default
-        var results: [ConfigFile] = []
-        for (name, relPath) in commonConfigs {
-            let filePath = (relPath.hasPrefix("/")) ? relPath : homePath + "/" + relPath
-            if fileManager.fileExists(atPath: filePath) {
-                results.append(ConfigFile(name: name, path: filePath, isCustom: false))
-            }
-        }
-        return results
-    }
+    
 
     private func togglePin(for file: ConfigFile) {
         if let index = configFiles.firstIndex(where: { $0.id == file.id }) {
